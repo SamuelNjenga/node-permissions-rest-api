a7bcfdb29273df4f03ca0f614ea36aa9f5b21cd1
Samuel Njenga <snjenga704@gmail.com>
2020-06-16 07:38:56 +0300

    Added userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'basic',
+    enum: ["basic", "supervisor", "admin"]
+  }
+})
+
+const User = mongoose.model('user', UserSchema)
+
+module.exports = User;
